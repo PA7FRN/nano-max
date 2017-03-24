c7646010bb70515042fabafcd4801abe4cc65226
Edwin Arts <edwin.arts@strukton.com>
2017-03-24 10:32:49 +0100

    feat: klik aan klik uit zonder debounce

--- nano_max.ino
+++ nano_max.ino
@@ -1,8 +1,12 @@
 const int pinBtnRow = 5; //ASTRIX row
 const int pinBtnCol = 6; //ASTRIX column
-const int pinLed    = 13;
+const int pinLed    = 13; // D13 = on board LED
+
+int buttonState = LOW;
 
 void setup() {
+  Serial.begin(9600); 
+
   pinMode(pinLed, OUTPUT);
   pinMode(pinBtnCol, OUTPUT);
   pinMode(pinBtnRow, INPUT_PULLUP);
@@ -10,8 +14,23 @@ void setup() {
 }
 
 void loop() {
-  int buttonState;
-  
-  buttonState = digitalRead(pinBtnRow);
-  digitalWrite(pinLed, !buttonState);
+  int newButtonState = digitalRead(pinBtnRow);
+  if (buttonState != newButtonState) {
+    Serial.print(String(millis()));
+    Serial.println(": button changed");
+    buttonState = newButtonState;
+    if (buttonState == LOW) {
+      togleLight();
+    }
+  }
 }
+
+void togleLight() {
+  if (digitalRead(pinLed) == LOW) {
+    digitalWrite(pinLed, HIGH);
+  }
+  else {
+    digitalWrite(pinLed, LOW);
+  }
+}
+
